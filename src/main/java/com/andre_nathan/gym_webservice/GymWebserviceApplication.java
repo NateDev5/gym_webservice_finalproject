011369a05648504556b3bc66adfaf316346ed339
NateDev5 <nate.bes@protonmail.com>
2026-04-05 12:56:17 -0400

    project structure done

---
+++ src/main/java/com/andre_nathan/gym_webservice/GymWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.andre_nathan.gym_webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymWebserviceApplication.class, args);
+    }
+
+}
